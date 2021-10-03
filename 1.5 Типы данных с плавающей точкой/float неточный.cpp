a4948b0a0fd4eb54eb9fca0dbd99b11da5091e72
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-03 15:38:18 +0500

    Create float неточный.cpp

---
+++ 1.5 Типы данных с плавающей точкой/float неточный.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main() {
+    long double x;
+    cin >> x;
+    long int answer = float(x) - x;
+    cout << answer;
+}
