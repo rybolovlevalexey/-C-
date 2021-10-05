aab68e2a8f2400407052f2895d70255fc37305b9
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-05 14:28:50 +0500

    Create Инверсии.cpp

---
+++ 1.7 Итоговое тестирование/Инверсии.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main() {
+    long int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a > b) + (a > c) + (a > d) + (b > c) + (b > d) + (c > d);
+}
