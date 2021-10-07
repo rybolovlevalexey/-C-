1bed60136d94b022bb0e820deb3f8a87a344310c
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-07 20:55:10 +0500

    Create Еще одна таблица истинности.cpp

---
+++ 2.1 Логические операции/Еще одна таблица истинности.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main() {
+    long int a, b, c;
+    cin >> a >> b >> c;
+    cout << ((!(!a && b || c) || (b || !a)) && ((!c && !b) && !(b || !a || c)));
+}
