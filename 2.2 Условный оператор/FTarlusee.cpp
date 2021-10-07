8ec820c8d21ea91f0dbb6225fa58a37e95e31d10
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-07 20:56:28 +0500

    Create FTarlusee.cpp

---
+++ 2.2 Условный оператор/FTarlusee.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a == 1){
+        cout << "True";
+    } else {
+        cout << "False";
+    }
+}
