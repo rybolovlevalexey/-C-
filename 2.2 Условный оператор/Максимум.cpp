b2e5fadc0662245f80a7448c728a322350f772ee
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-07 20:56:49 +0500

    Create Максимум.cpp

---
+++ 2.2 Условный оператор/Максимум.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a > b){
+        cout << a;
+    } else {
+        cout << b;
+    }
+}
