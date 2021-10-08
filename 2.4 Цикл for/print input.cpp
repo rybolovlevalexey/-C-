e4c893b8bc69722a3b3eab6fa012810fa0da80c7
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-08 13:16:38 +0500

    Create print input.cpp

---
+++ 2.4 Цикл for/print input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, x;
+    cin >> n;
+    while (cin >> x){
+        cout << x << " ";
+    }
+}
