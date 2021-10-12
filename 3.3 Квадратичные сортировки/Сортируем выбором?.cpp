15c982f9e1491d92154753c4629ad0b860728b01
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-12 15:07:36 +0500

    Create Сортируем выбором?.cpp

---
+++ 3.3 Квадратичные сортировки/Сортируем выбором?.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n, count = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++){
+        int jmin = i;
+        for (int j = i + 1; j < n; j++){
+            if (a[jmin] > a[j]){
+                jmin = j;
+            }
+        }
+        swap(a[i], a[jmin]);
+        count += 1;
+    }
+    cout << count;
+}
