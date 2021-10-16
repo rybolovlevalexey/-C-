91c5d3bb90ba5ca1f6365e4bd7044c360911aff1
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-10-16 19:00:22 +0500

    Create Парты.cpp

---
+++ Введение в программирование (C++)/1.3 Целые числа/Парты.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, c, d, e, f, g;
+    cin >> a >> b >> c;
+    d = (a + 1) / 2;
+    e = (b + 1) / 2;
+    f = (c + 1) / 2;
+    g = d + e + f;
+    cout << g;
+  return 0;
+}
