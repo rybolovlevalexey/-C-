989399d7e8143201af9b1fb4f3fe27f7ebb91b02
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2021-11-03 09:22:56 +0500

    Create Задание №2.cpp

---
+++ Введение в программирование (C++)/1.6 Действительные числа/Задание №2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+int main() {
+    int c, d;
+        double a, b;
+    cin >> a;
+    b = a * 10;
+    c = trunc(b);
+    d = c % 10;
+    cout << d;
+  return 0;
+}
